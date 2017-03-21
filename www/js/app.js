0aa1be7dc90a046f3295bc9a105411c8688e7f48
felixottander <felix.ottander@yahoo.com>
2017-03-21 11:13:52 +0100

    In app.js sets the default tab to about.

--- www/js/app.js
+++ www/js/app.js
@@ -35,6 +35,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     })
 
 
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/about');
 
 });
